6f09d0a0ce71cf3a005d0ed0261fc05874a0c9ca
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-04-28 11:48:27 +0100

    set-operators

--- Employee.Linq/Program.cs
+++ Employee.Linq/Program.cs
@@ -9,75 +9,97 @@ namespace Employee.Linq
     {
         static void Main()
         {
-            var emp = new List<Employee>();
-            emp.Add(new Employee { Id = 0001, Name = "Adekunle Victor", Age = 24, Gender = "Male", Salary = 150000 });
-            emp.Add(new Employee { Id = 0002, Name = "Olabisi Esther ", Age = 21, Gender = "Female", Salary = 250000 });
-            emp.Add(new Employee { Id = 0003, Name = "Adebimpe Morayo", Age = 20, Gender = "Female", Salary = 200000 });
-            emp.Add(new Employee { Id = 0004, Name = "Obafemi Olaolu", Age = 26, Gender = "Male", Salary = 700000 });
-            emp.Add(new Employee { Id = 0005, Name = "Nwafor Chisom", Age = 27, Gender = "Male", Salary = 250000 });
-            emp.Add(new Employee { Id = 0006, Name = "Adeyemi Richard", Age = 29, Gender = "Male", Salary = 300000 });
-            emp.Add(new Employee { Id = 0007, Name = "Lawal Victoria", Age = 22, Gender = "Feale", Salary = 100000 });
-            emp.Add(new Employee { Id = 0008, Name = "Adeniyi Esther", Age = 21, Gender = "Female", Salary = 190000 });
-            emp.Add(new Employee { Id = 0009, Name = "Bamidele Segun", Age = 30, Gender = "Male", Salary = 350000 });
-            emp.Add(new Employee { Id = 0010, Name = "Olatunde Bukunmi", Age = 26, Gender = "Female", Salary = 500000 });
+            int[] evennumbers = { 2, 4, 4, 6, 8, 10, 12, 14, 16, 18, 19, 20, 8, 19, 11 };
+            int [] oddnumbers = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 18, 2, 12};
+            //string[] words = { "words", "all", "come", "those" };
+
+            //Union
+
+            var allnumber = evennumbers.Union(oddnumbers)
+                                        .OrderBy(x => x);
+            //                            .Sum(x => x);
+            //Console.WriteLine(allnumber);
+                                        
+            
+
+            var numbers = evennumbers.Where(x => x > 10)
+                                        .Union(evennumbers.Where(x => x < 10));
 
 
-            var work = emp.Where(x => x.Age > 25)
-                           .OrderByDescending(x => x.Age);
-
-            foreach (var employee in work)
+            foreach (var i in allnumber)
             {
-                Console.WriteLine($"ID: {employee.Id} : Name:{employee.Name} Age: {employee.Age} : Gender: {employee.Gender} :" +
-                    $"Salary: {employee.Salary}");
+                Console.WriteLine(i);
             }
 
 
-            var work2 = emp.Where(x => x.Gender == "Female")
-                           .OrderBy(x => x.Gender);
-
             Console.WriteLine();
-
-            foreach(var employee in work2)
+            //Intersect
+            var queuenumber = evennumbers.Intersect(oddnumbers)
+                                          .OrderBy(x => x);
+            
+            foreach(var i in queuenumber)
             {
-                Console.WriteLine($"ID: {employee.Id} : Name:{employee.Name} Age: {employee.Age} : Gender: {employee.Gender} :" +
-                    $"Salary: {employee.Salary}");
-            }
-
-            Console.WriteLine();
-
-            var work3 = emp.Where(x => (x.Age == 20) || (x.Age == 30) || (x.Age == 35))
-                           .OrderBy(x => x.Name);
-
-            foreach(var employee in work3)
-            {
-                Console.WriteLine($"ID: {employee.Id} : Name:{employee.Name} Age: {employee.Age} : Gender: {employee.Gender} :" +
-                    $"Salary: {employee.Salary}");
+                Console.WriteLine($"Intersect: {i}");
             }
 
             Console.WriteLine();
 
+            //Except
 
-            var work4 = emp.Where(x => x.Id <= 0008 && x.Gender == "Female" )
-                            .OrderBy(x => x.Gender == "Female"  );
-
-
-            foreach(var employee in work4)
+            var exceptnumber = oddnumbers.Except(evennumbers);
+            foreach(var i in exceptnumber)
             {
-                Console.WriteLine($"ID: {employee.Id} : Name:{employee.Name} Age: {employee.Age} : Gender: {employee.Gender} :" +
-                    $"Salary: {employee.Salary}");
+                Console.WriteLine(i);
             }
+
+            Console.WriteLine();
+            //Distict
+
+            var distinctnumber = evennumbers.Distinct();
+            foreach(var i in distinctnumber)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            //sum 
+             var sumnumbers = evennumbers.Where(x => x < 20)
+                                           .Sum(x => x);
+            Console.WriteLine($"Sumnumbers: {sumnumbers}");
+
+            Console.WriteLine();
+
+
+            //min
+            var number = oddnumbers.Min(x => x);
+            Console.WriteLine(number);
+
+            Console.WriteLine();
+
+            //max
+
+            var num = evennumbers.Max(x => x);
+            Console.WriteLine(num);
+            Console.WriteLine();
+
+            //average 
+
+            var num1 = evennumbers.Average(x => x);
+            Console.WriteLine(num1);
+
+
+                Console.WriteLine();
+
+            //Aggregate 
+             var aggsum = evennumbers.Aggregate((a, b) => a + b);
+            Console.WriteLine(aggsum);
+           
+
+            //
+
+
+         
 
         }
-
-       
-    }
-
-    public class Employee
-    {
-        public int Id { get; set; } 
-        public string Name { get; set; }
-        public int Age { get; set; }   
-        public string Gender { get; set; } 
-        public double Salary { get; set; }  
     }
 }
