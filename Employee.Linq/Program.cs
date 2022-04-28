48e43d5a1030e1298fa3ffe47ffb443853017fda
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-04-28 22:32:58 +0100

    i dont know what this is

--- Employee.Linq/Program.cs
+++ Employee.Linq/Program.cs
@@ -13,91 +13,105 @@ namespace Employee.Linq
             int [] oddnumbers = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 18, 2, 12};
             //string[] words = { "words", "all", "come", "those" };
 
-            //Union
+            ////Union
 
-            var allnumber = evennumbers.Union(oddnumbers)
-                                        .OrderBy(x => x);
-            //                            .Sum(x => x);
-            //Console.WriteLine(allnumber);
-                                        
-            
-
-            var numbers = evennumbers.Where(x => x > 10)
-                                        .Union(evennumbers.Where(x => x < 10));
+            //var allnumber = evennumbers.Union(oddnumbers)
+            //                            .OrderBy(x => x);
+            ////                            .Sum(x => x);
+            ////Console.WriteLine(allnumber);
 
 
-            foreach (var i in allnumber)
-            {
-                Console.WriteLine(i);
-            }
 
+            //var numbers = evennumbers.Where(x => x > 10)
+            //                            .Union(evennumbers.Where(x => x < 10));
+
+
+            //foreach (var i in allnumber)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Console.WriteLine();
+            ////Intersect
+            //var queuenumber = evennumbers.Intersect(oddnumbers)
+            //                              .OrderBy(x => x);
+
+            //foreach(var i in queuenumber)
+            //{
+            //    Console.WriteLine($"Intersect: {i}");
+            //}
+
+            //Console.WriteLine();
+
+            ////Except
+
+            //var exceptnumber = oddnumbers.Except(evennumbers);
+            //foreach(var i in exceptnumber)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine();
+            ////Distict
+
+            //var distinctnumber = evennumbers.Distinct();
+            //foreach(var i in distinctnumber)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine();
+            ////sum 
+            // var sumnumbers = evennumbers.Where(x => x < 20)
+            //                               .Sum(x => x);
+            //Console.WriteLine($"Sumnumbers: {sumnumbers}");
+
+            //Console.WriteLine();
+
+
+            ////min
+            //var number = oddnumbers.Min(x => x);
+            //Console.WriteLine(number);
+
+            //Console.WriteLine();
+
+            ////max
+
+            //var num = evennumbers.Max(x => x);
+            //Console.WriteLine(num);
+            //Console.WriteLine();
+
+            ////average 
+
+            //var num1 = evennumbers.Average(x => x);
+            //Console.WriteLine(num1);
+
+
+            //    Console.WriteLine();
+
+            ////Aggregate 
+            // var aggsum = evennumbers.Aggregate((a, b) => a + b);
+            //Console.WriteLine(aggsum);
+
+
+            // ELEMENT OPERATORS
+                
+            //first or FirstOrDefault
+            var numbers = evennumbers.FirstOrDefault();
+            Console.WriteLine(numbers);
+            Console.WriteLine();
+
+            //Last or lastordefault 
+
+            var allodds = oddnumbers.LastOrDefault();
+            Console.WriteLine(allodds);
 
             Console.WriteLine();
-            //Intersect
-            var queuenumber = evennumbers.Intersect(oddnumbers)
-                                          .OrderBy(x => x);
-            
-            foreach(var i in queuenumber)
-            {
-                Console.WriteLine($"Intersect: {i}");
-            }
 
-            Console.WriteLine();
-
-            //Except
-
-            var exceptnumber = oddnumbers.Except(evennumbers);
-            foreach(var i in exceptnumber)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine();
-            //Distict
-
-            var distinctnumber = evennumbers.Distinct();
-            foreach(var i in distinctnumber)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine();
-            //sum 
-             var sumnumbers = evennumbers.Where(x => x < 20)
-                                           .Sum(x => x);
-            Console.WriteLine($"Sumnumbers: {sumnumbers}");
-
-            Console.WriteLine();
-
-
-            //min
-            var number = oddnumbers.Min(x => x);
-            Console.WriteLine(number);
-
-            Console.WriteLine();
-
-            //max
-
-            var num = evennumbers.Max(x => x);
-            Console.WriteLine(num);
-            Console.WriteLine();
-
-            //average 
-
-            var num1 = evennumbers.Average(x => x);
-            Console.WriteLine(num1);
-
-
-                Console.WriteLine();
-
-            //Aggregate 
-             var aggsum = evennumbers.Aggregate((a, b) => a + b);
-            Console.WriteLine(aggsum);
-           
-
-            //
-
-
+            //single or SingleOrDefault
+            var all = oddnumbers.SingleOrDefault(x => x > 18);
+            Console.WriteLine(all);
          
 
         }
