83450685e163013cfcccb7a54e7feb63a7e67a9d
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-05-04 14:09:00 +0100

    all, select many...

--- Employee.Linq/Program.cs
+++ Employee.Linq/Program.cs
@@ -9,9 +9,11 @@ namespace Employee.Linq
     {
         static void Main()
         {
+            var Student = student.GetStudents();   
             int[] evennumbers = { 2, 4, 4, 6, 8, 10, 12, 14, 16, 18, 19, 20, 8, 19, 11 };
             int [] oddnumbers = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 18, 2, 12};
-            //string[] words = { "words", "all", "come", "those" };
+            int[] onenumbers = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            string[] words = { "chalk", "Pencil", "marker" };
 
             ////Union
 
@@ -53,7 +55,7 @@ namespace Employee.Linq
             //}
 
             //Console.WriteLine();
-            ////Distict
+            //Distict
 
             //var distinctnumber = evennumbers.Distinct();
             //foreach(var i in distinctnumber)
@@ -62,7 +64,7 @@ namespace Employee.Linq
             //}
 
             //Console.WriteLine();
-            ////sum 
+            //sum 
             // var sumnumbers = evennumbers.Where(x => x < 20)
             //                               .Sum(x => x);
             //Console.WriteLine($"Sumnumbers: {sumnumbers}");
@@ -70,19 +72,19 @@ namespace Employee.Linq
             //Console.WriteLine();
 
 
-            ////min
+            //min
             //var number = oddnumbers.Min(x => x);
             //Console.WriteLine(number);
 
             //Console.WriteLine();
 
-            ////max
+            //max
 
             //var num = evennumbers.Max(x => x);
             //Console.WriteLine(num);
             //Console.WriteLine();
 
-            ////average 
+            //average 
 
             //var num1 = evennumbers.Average(x => x);
             //Console.WriteLine(num1);
@@ -90,30 +92,82 @@ namespace Employee.Linq
 
             //    Console.WriteLine();
 
-            ////Aggregate 
+            //Aggregate 
             // var aggsum = evennumbers.Aggregate((a, b) => a + b);
             //Console.WriteLine(aggsum);
 
 
             // ELEMENT OPERATORS
-                
+
             //first or FirstOrDefault
-            var numbers = evennumbers.FirstOrDefault();
-            Console.WriteLine(numbers);
-            Console.WriteLine();
+            //var numbers = evennumbers.FirstOrDefault();
+            //Console.WriteLine(numbers);
+            //Console.WriteLine();
 
             //Last or lastordefault 
 
-            var allodds = oddnumbers.LastOrDefault();
-            Console.WriteLine(allodds);
+            //var allodds = oddnumbers.LastOrDefault();
+            //Console.WriteLine(allodds);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
             //single or SingleOrDefault
-            var all = oddnumbers.SingleOrDefault(x => x > 18);
-            Console.WriteLine(all);
+            //var all = oddnumbers.SingleOrDefault(x => x > 18);
+            //Console.WriteLine(all);
+
+
+
+            //Quantifiers
+            //Any
+            var numbers = oddnumbers.Any(x => x == 3);
+            Console.WriteLine(numbers);
+
+            var keyword = words.Any(x => x == "Marker");
+            Console.WriteLine(keyword);
+
+            //All
+             var newnumbers = onenumbers.All(x => x == 9);
+            Console.WriteLine(newnumbers);
+
+            //select many
+
+            var items = Student.SelectMany(x => x.course,
+                                            (studentsname, dept) => new
+                                            {
+                                                newstudentname = studentsname.name,
+                                                Course = dept
+                                                });
+            foreach(var item in items)
+            {
+                Console.WriteLine($"{item.Course} : {item.newstudentname}");
+            }
+
+            Console.WriteLine();
+            var product = Student.ToDictionary<student, int>(s => s.ID);
+            foreach(var item in product.Keys)
+            {
+                Console.WriteLine("key {0} value {1}", item, product[item].ID);
+            }
+
          
 
+        }
+    }
+    public class student
+    {
+        public string name { get; set; }
+        public string place { get; set; }
+        public List<string> course { get; set; }
+        public int ID {get; set; }
+
+        public static List<student> GetStudents()
+        {
+            return new List<student>
+            {
+                new student { ID = 1, name = "John", place = "Lagos", course = new List<String>(){"biology", "chemistry", "maths" } },
+                new student { ID = 2, name = "Edward", place = "Ibadan", course = new List<string>(){"chem104", "bio101", "bam106" } },
+                new student { ID = 3, name = "Ali", place = "Jos", course = new List<string>(){"chem404", "bio401", "bam206" } },
+            };
         }
     }
 }
